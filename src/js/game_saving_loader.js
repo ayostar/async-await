5493270506abb9fa68657cbc43e9384b939e591d
Artem Starodubtsev <ayostar@gmail.com>
2022-10-18 08:12:24 +0300

    reader back to original

--- src/js/game_saving_loader.js
+++ src/js/game_saving_loader.js
@@ -3,10 +3,8 @@ import json from './parser';
 import GameSaving from './game_saving';
 
 export default class GameSavingLoader {
-  static async load(
-    data = '{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}'
-  ) {
-    const savingData = await json(await read(data));
+  static async load() {
+    const savingData = await json(await read());
     const result = JSON.parse(savingData);
     return new GameSaving(result);
   }
